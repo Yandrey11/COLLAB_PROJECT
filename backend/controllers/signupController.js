1db56f27792bbb9a318ce12868801680badf460b
unknown <2301104291@student.buksu.edu.ph>
2025-10-25 22:00:31 +0800

    2nd Commit with google

---
+++ backend/controllers/signupController.js
@@ -0,0 +1,27 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = jwt.sign(
+      { id: user._id, name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ message: "Signup successful", result: user, token });
+  } catch (error) {
+    console.error("Signup Error:", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
