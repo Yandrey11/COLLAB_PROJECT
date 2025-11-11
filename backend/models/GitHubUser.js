9def5b676a14179077258daab5c5970c7ed9aacf
unknown <2301104291@student.buksu.edu.ph>
2025-11-11 17:47:35 +0800

    Added gitlogin

--- backend/models/GitHubUser.js
+++ backend/models/GitHubUser.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const GitHubUserSchema = new mongoose.Schema({
   githubId: { type: String, required: true, unique: true },
   username: String,
-  email: String,
-  avatar: String,
+  avatar: String
 });
 
-const GitHubUser = mongoose.model("GitHubUser", GitHubUserSchema);
-export default GitHubUser;
+export default mongoose.model("GitHubUser", GitHubUserSchema);
